4eec581eee1a423a160e2e5e0e6d272483a224d9
a.pushkin <a.v.pushkin@gmail.com>
2022-10-31 18:30:28 +0300

    initial

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+from collections import OrderedDict
 import re
-from pprint import pprint
 import csv
 with open("phonebook_raw.csv", encoding="utf-8") as f:
   rows = csv.reader(f, delimiter=",")
@@ -14,9 +14,26 @@ for line in contacts_list:
 
   result.append(new_raw.split(','))
 
+for contact in result:
+    if contact[-2] == '+7()-- доб. ':
+        contact[-2] = ''
 
 
+def remove_duplicates(list_: list) -> list:
+    k = 0
+    while k < len(list_) - 1:
+        for list1, list2 in zip(list_[k], list_[k + 1]):
+            if list1 == list2:
+                new_list = list(OrderedDict.fromkeys(list_[k] + list_[k + 1]))
+                list_.remove(list_[k + 1])
+                list_.remove(list_[k])
+                list_.append(new_list)
+            break
+        k += 1
+    return list_
+
+unique_data_list = remove_duplicates(result)
 
 with open("phonebook.csv", "w", encoding="utf-8") as f:
   datawriter = csv.writer(f, delimiter=',')
-  datawriter.writerows(result)
+  datawriter.writerows(unique_data_list)
