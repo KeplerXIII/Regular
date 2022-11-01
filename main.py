2256fb53988ba07e68848e540ace756184d3f7d0
a.pushkin <a.v.pushkin@gmail.com>
2022-11-01 11:51:55 +0300

    Update

--- main.py
+++ main.py
@@ -1,40 +1,53 @@
 from collections import OrderedDict
 import re
+from pprint import pprint
 import csv
 with open("phonebook_raw.csv", encoding="utf-8") as f:
   rows = csv.reader(f, delimiter=",")
   contacts_list = list(rows)
 
 result = []
+dict = [{'lastname':'lastname','firstname':'firstname','surname':'surname','organization':'organization','position':'position','phone':'phone','email':'email'}]
 pattern = r"([А-Я]\w+).([А-Я]\w+).(([А-Я]\w+)|(,+)),+(([А-Яа-я]\w+)|),+(([^+78,a-zA-Z]+)|)(,+|)((\+7|8)?(\s|)(\(|)((?:\d{1,3}))(\)|)(\D|)((?:\d{1,3}))(\D|)((?:\d{1,2}))(\D|)((?:\d{1,2}))|)(\W|)((\(|)доб. (....)(\)|)|)(,|)((\w+(.|)\w+@\w+(.|).\w+(.|))|)"
+
 for line in contacts_list:
   line_text = ",".join(line)
-  new_raw = re.sub(pattern, r"\1,\2,\4,\7,\9,+7(\15)\18-\20-\22 доб. \26,\30", line_text)
-
-  result.append(new_raw.split(','))
-
-# пришлось добавить цикл иначе дубликаты наслаивались из-за непустого значения
-for contact in result:
-    if contact[-2] == '+7()-- доб. ':
-        contact[-2] = ''
+  new_raw = re.sub(pattern, r"\1,\2,\4,\7,\9,+7(\15)\18-\20-\22 доб. \26,\30", line_text).split(',')
 
 
+  if new_raw[-2] == '+7()-- доб. ':
+      new_raw[-2] = ''
 
-def remove_duplicates(list_: list) -> list:
-    k = 0
-    while k < len(list_) - 1:
-        for list1, list2 in zip(list_[k], list_[k + 1]):
-            if list1 == list2:
-                new_list = list(OrderedDict.fromkeys(list_[k] + list_[k + 1]))
-                list_.remove(list_[k + 1])
-                list_.remove(list_[k])
-                list_.append(new_list)
-            break
-        k += 1
-    return list_
+  new_raw_dict = {'lastname': new_raw[0], 'firstname': new_raw[1], 'surname': new_raw[2], 'organization': new_raw[3], 'position': new_raw[4], 'phone': new_raw[5], 'email': new_raw[6]}
+  for contact in dict:
+    if new_raw[0] == contact['lastname'] and new_raw[1] == contact['firstname']:
+        if contact['email'] == '':
+            contact['email'] = new_raw[6]
+        if contact['phone'] == '':
+            contact['phone'] = new_raw[5]
+        if contact['position'] == '':
+            contact['position'] = new_raw[4]
+        if contact['organization'] == '':
+            contact['organization'] = new_raw[3]
+        trigger = 0
 
-unique_data_list = remove_duplicates(result)
+        break
+    else:
+      trigger = 1
+  if trigger == 1:
+    dict.append({'lastname': new_raw[0], 'firstname': new_raw[1], 'surname': new_raw[2], 'organization': new_raw[3], 'position': new_raw[4], 'phone': new_raw[5], 'email': new_raw[6]})
+  else:
+    pass
+
+for contact in dict:
+    result.append([contact['lastname'], contact['firstname'], contact['surname'], contact['organization'], contact['position'], contact['phone'], contact['email']])
+
+
+
+
+
+
 
 with open("phonebook.csv", "w", encoding="utf-8") as f:
   datawriter = csv.writer(f, delimiter=',')
-  datawriter.writerows(unique_data_list)
+  datawriter.writerows(result)
